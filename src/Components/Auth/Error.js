74377549fefab4e76cd655200c59b3b722ccf153
FIRST_NAME LAST_NAME <MY_NAME@example.com>
2020-07-19 20:12:03 +0200

    Change Error component

--- src/Components/Auth/Error.js
+++ src/Components/Auth/Error.js
@@ -1,13 +1,13 @@
 import React from 'react';
-import { Message } from 'semantic-ui-react';
-
-
+import { Segment } from 'semantic-ui-react';
 
 export default function Error({ text }) {
-    return (
-        <Message negative>
-            <Message.Header>{text}</Message.Header>
-            <h4>Write correct data to go next</h4>
-        </Message>
-    );
+	return (
+		<div>
+			<Segment negative size="tiny" textAlign="center">
+				<h1>{text}</h1>
+				<h4>Write correct data to go next</h4>
+			</Segment>
+		</div>
+	);
 }
